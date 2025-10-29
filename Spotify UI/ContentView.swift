ac3fde6a18881b5f00b0feb5c3f487a529e9c664
Andy Huoy <anhuo@unc.edu>
2025-10-29 13:28:47 -0400

    Initial Commit

---
+++ Spotify UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotify UI
+//
+//  Created by Andy Huoy on 10/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spotify UI/Spotify_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_UIApp.swift
+//  Spotify UI
+//
+//  Created by Andy Huoy on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
