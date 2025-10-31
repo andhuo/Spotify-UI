d60436a2eb08b795fc48ee0111f72931f4343e0d
Andy Huoy <anhuo@unc.edu>
2025-10-31 13:40:53 -0400

    Album cover upload and text + Scaling Fixed in SongView

---
+++ Spotify UI/SongView.swift
@@ -0,0 +1,36 @@
+//
+//  SongView.swift
+//  Spotify UI
+//
+//  Created by Andy Huoy on 10/29/25.
+//
+
+import SwiftUI
+
+struct SongView: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Image("Daniel Caesar Album Cover")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .padding()
+            }
+            
+            Text("Hold me Down")
+                .font(.headline)
+                .padding()
+            
+            Text("Daniel Caesar")
+                .font(.caption)
+                .padding()
+            
+        }
+
+    }
+}
+
+#Preview {
+    SongView()
+}
