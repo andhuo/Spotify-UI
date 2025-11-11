04d4c5aa6652881f9331b5738ba3a46fdacce5e7
Andy Huoy <anhuo@unc.edu>
2025-11-11 09:49:18 -0500

    Adjusted SongView and polished songname and author

--- Spotify UI/SongView.swift
+++ Spotify UI/SongView.swift
@@ -10,30 +10,47 @@ import SwiftUI
 struct SongView: View {
     var body: some View {
         
-        VStack{
-            HStack{
+        VStack(spacing: 24){
                 Image("Daniel Caesar Album Cover")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 300, height: 300)
+                    .frame(width: 340, height: 340)
                     .cornerRadius(10)
-                    .padding()
+                    .padding(.top, 10)
+            
+            
+            HStack(alignment: .top) {
+                VStack(alignment: .leading, spacing: 6) {
+                    Text("Hold Me Down")
+                        .font(.system(size: 22, weight: .semibold))
+                            .foregroundColor(.white)
+                            .padding(.bottom, 2)
+                              
+                    Text("Daniel Caesar")
+                            .font(.system(size: 16))
+                            .foregroundColor(.gray)
+                            
+                            
+                }
+                
+                Spacer()
+                
+                Button(action: {}) {
+                    Image(systemName: "plus.circle")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .padding(.leading, 12)
+                }
             }
-            
-            
-            Text("Hold Me Down")
-                .font(.headline)
-                .padding()
-            
-            Text("Daniel Caesar")
-                .font(.caption)
-                .padding()
-            
+            .padding(.horizontal, 40)
         }
-
     }
 }
-
 #Preview {
-    SongView()
+    ZStack {
+        LinearGradient(colors: [.gray.opacity(0.4), .black.opacity(0.9)], startPoint: .top, endPoint: .bottom)
+            .ignoresSafeArea()
+        SongView()
+    }
+    .preferredColorScheme(.dark)
 }
